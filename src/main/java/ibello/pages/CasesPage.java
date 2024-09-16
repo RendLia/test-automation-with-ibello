8b9c1316b49fd65c0e4b2a44af2193b48b9dae6f
Lívia Rendi <livia.rendi@qlmdc.hu>
2024-09-16 15:30:41 +0200

    Ügyek oldalleíró létre hozzása

---
+++ src/main/java/ibello/pages/CasesPage.java
@@ -0,0 +1,22 @@
+package ibello.pages;
+
+import hu.ibello.core.Name;
+import hu.ibello.elements.WebElement;
+import hu.ibello.pages.PageObject;
+import hu.ibello.search.By;
+import hu.ibello.search.Find;
+import hu.ibello.search.Relation;
+import hu.ibello.search.RelationType;
+
+@Name("Cases Page")
+public class CasesPage extends PageObject {
+
+	@Find(by = By.TEXT, using = "Ügyek")
+	@Relation(type = RelationType.DESCENDANT_OF, by = By.CLASS_NAME, using = "panel-heading")
+	private WebElement cím;
+
+	public void page_must_be_loaded() {
+		expectations().expect(cím).toBe().displayed();
+	}
+
+}
